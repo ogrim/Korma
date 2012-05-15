8622e47eefc19fea6f031e0e9a4f2d6eac2598dc
Aleksander Skjæveland Larsen <aleksander@ogrim.no>
2012-05-15 11:06:15 +0200

    Updating to Clojure 1.4, and adding my group id

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject korma "0.3.0-beta10"
+(defproject org.clojars.ogrim/korma "0.3.0-beta10"
   :description "Tasty SQL for Clojure"
   :url "http://github.com/ibdknox/korma"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [c3p0/c3p0 "0.9.1.2"]
                  [org.clojure/java.jdbc "0.2.1"]]
   :codox {:exclude [korma.sql.engine korma.sql.fns korma.sql.utils]}
